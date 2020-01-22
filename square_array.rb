e7136651c51185ac403820585880b8d7370055e3
Charles Pelton <charliejp0311@gmail.com>
2020-01-22 14:12:18 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0 
   array.each do |num|
     #array[counter] << num**2
-    array[counter] << num**2
+    array[counter] = num**2
     pp array[counter]
     counter += 1 
   end
