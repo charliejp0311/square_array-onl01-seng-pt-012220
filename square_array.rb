295afcd4d797724dac2b8d66e22bdca39fd46a0a
Charles Pelton <charliejp0311@gmail.com>
2020-01-22 14:12:45 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,9 +2,7 @@ def square_array(array)
   # your code here
   counter = 0 
   array.each do |num|
-    #array[counter] << num**2
     array[counter] = num**2
-    pp array[counter]
     counter += 1 
   end
   array
